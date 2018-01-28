38ae53007e3566d0c2bc055e7da6875518e9f53f
Wesley Cabus <wesley@gotsharp.be>
2018-01-28 13:52:55 +0100

    Added IDP project

---
+++ src/Sprotify.IDP/Config.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Sprotify.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = Guid.NewGuid().ToString(),
+                    Username = "wesley",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Wesley"),
+                        new Claim("family_name", "Cabus"),
+                        new Claim("address", "Antwerp"),
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new []
+            {
+                new Client
+                {
+                    ClientId = "sprotifyclient",
+                    ClientName = "Sprotify",
+
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Address,
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(), 
+                new IdentityResources.Profile(), 
+                new IdentityResources.Address(), 
+                new IdentityResources.Phone(), 
+                new IdentityResources.Email(), 
+            };
+        }
+    }
+}
