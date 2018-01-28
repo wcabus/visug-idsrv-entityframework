9e18d24b2f5594e4f080c99228cb8333a37edafe
Wesley Cabus <wesley@gotsharp.be>
2018-01-28 17:04:50 +0100

    IdSrv databases

--- src/Sprotify.IDP/Startup.cs
+++ src/Sprotify.IDP/Startup.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Linq;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -7,18 +13,36 @@ namespace Sprotify.IDP
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
 
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
-                .AddInMemoryClients(Config.GetClients())
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApiResources())
-                .AddTestUsers(Config.GetUsers());
+                .AddTestUsers(Config.GetUsers())
+                // Clients, resources, ...
+                .AddConfigurationStore(x =>
+                {
+                    x.ConfigureDbContext = dbco => dbco.UseSqlServer(Configuration.GetConnectionString("IDPDB"),
+                        options => options.MigrationsAssembly(migrationsAssembly));
+                })
+                // Tokens, codes, consents, ...
+                .AddOperationalStore(x =>
+                {
+                    x.ConfigureDbContext = dbco => dbco.UseSqlServer(Configuration.GetConnectionString("IDPDB"),
+                        options => options.MigrationsAssembly(migrationsAssembly));
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,14 +53,58 @@ namespace Sprotify.IDP
 
             if (env.IsDevelopment())
             {
+                app.UseDatabaseErrorPage();
                 app.UseDeveloperExceptionPage();
             }
+
+            InitializeDatabase(app);
 
             app.UseStaticFiles();
 
             app.UseIdentityServer();
 
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>()
+                    .Database.Migrate();
+
+                using (var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>())
+                {
+                    context.Database.Migrate();
+
+                    if (!context.Clients.Any())
+                    {
+                        foreach (var client in Config.GetClients())
+                        {
+                            context.Clients.Add(client.ToEntity());
+                        }
+                        context.SaveChanges();
+                    }
+
+                    if (!context.IdentityResources.Any())
+                    {
+                        foreach (var resource in Config.GetIdentityResources())
+                        {
+                            context.IdentityResources.Add(resource.ToEntity());
+                        }
+                        context.SaveChanges();
+                    }
+
+                    if (!context.ApiResources.Any())
+                    {
+                        foreach (var resource in Config.GetApiResources())
+                        {
+                            context.ApiResources.Add(resource.ToEntity());
+                        }
+                        context.SaveChanges();
+                    }
+                }
+            }
         }
     }
 }
